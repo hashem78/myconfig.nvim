917ac0fee99dbf1ab6ea8712e215715535c9985f
hashem78 <hashem.olayano@gmail.com>
2024-01-02 01:53:15 +0300

    Move YankHightlight setup out of telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,13 +1,13 @@
--- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
-})
+
+
+
+
+
+
+
+
+
+
 
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
