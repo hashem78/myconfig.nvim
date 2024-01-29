0101fd73892855192728c8c22734536117feda5f
hashem78 <hashem.olayano@gmail.com>
2024-01-30 00:28:06 +0300

    add general options

--- lua/hashem/remap.lua
+++ lua/hashem/remap.lua
@@ -1,6 +1,9 @@
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 
+vim.o.expandtab = true
+vim.o.autoindent = true
+vim.o.smartindent = true
 -- [[ Setting options ]]
 -- See `:help vim.o`
 -- NOTE: You can change these options as you wish!
