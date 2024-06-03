12b6e1c8df6302899a91bd68188f98392400b16b
Hashem Olayan <holayan@atypon.com>
2024-06-03 14:29:21 +0300

    add back format on save

--- lua/plugins/nvim-lspconfing.lua
+++ lua/plugins/nvim-lspconfing.lua
@@ -42,6 +42,17 @@ return {
 		'folke/neodev.nvim',
 	},
 	config = function()
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+			callback = function(args)
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					buffer = args.buf,
+					callback = function()
+						vim.lsp.buf.format { async = false, id = args.data.client_id }
+					end,
+				})
+			end
+		})
 		require('mason').setup()
 		require('mason-lspconfig').setup()
 		require('neodev').setup()
