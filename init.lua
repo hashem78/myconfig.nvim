93c3fc4e91a97c60bd99bf8fc6aa6b9309188385
hashem78 <hashem.olayano@gmail.com>
2024-08-15 09:15:38 +0300

    use catppuccin colorscheme

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.opt.cmdheight = 0
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,14 +1,17 @@
 return {
-
-	-- Set lualine as statusline
-	'nvim-lualine/lualine.nvim',
-	-- See `:help lualine.txt`
+	"nvim-lualine/lualine.nvim",
 	opts = {
 		options = {
 			icons_enabled = false,
-			theme = 'onedark',
+			theme = 'catppuccin',
 			component_separators = '|',
 			section_separators = '',
 		},
 	},
+	config = function(_, opts)
+		require('lualine').setup(opts)
+		if os.getenv('TMUX') then
+			vim.defer_fn(function() vim.o.laststatus = 0 end, 0)
+		end
+	end
 }
