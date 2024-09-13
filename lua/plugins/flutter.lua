0fb00caba58ad3af7fcac99de4c155c6683a51db
hashem78 <hashem.olayano@gmail.com>
2024-09-13 20:36:58 +0300

    better formatting

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -27,11 +27,6 @@ local on_attach = function(client, bufnr)
 	nmap('<leader>wl', function()
 		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 	end, '[W]orkspace [L]ist Folders')
-
-
-	vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-		vim.lsp.buf.format()
-	end, { desc = 'Format current buffer with LSP' })
 end
 return {
 	'akinsho/flutter-tools.nvim',
--- lua/plugins/nvim-lspconfing.lua
+++ lua/plugins/nvim-lspconfing.lua
@@ -27,11 +27,6 @@ local on_attach = function(client, bufnr)
 	nmap('<leader>wl', function()
 		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 	end, '[W]orkspace [L]ist Folders')
-
-
-	vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-		vim.lsp.buf.format()
-	end, { desc = 'Format current buffer with LSP' })
 end
 
 return {
@@ -43,17 +38,6 @@ return {
 		'folke/neodev.nvim',
 	},
 	config = function()
-		vim.api.nvim_create_autocmd("LspAttach", {
-			group = vim.api.nvim_create_augroup("lsp", { clear = true }),
-			callback = function(args)
-				vim.api.nvim_create_autocmd("BufWritePre", {
-					buffer = args.buf,
-					callback = function()
-						vim.lsp.buf.format { async = false, id = args.data.client_id }
-					end,
-				})
-			end
-		})
 		require('mason').setup()
 		require('mason-lspconfig').setup()
 		require('neodev').setup()
